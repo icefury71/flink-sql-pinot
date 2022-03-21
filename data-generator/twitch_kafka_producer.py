776fd33fa7cc6bf80414504729f02080d94638ff
Chinmay Soman <chinmay.cerebro@gmail.com>
2022-03-20 22:24:42 -0700

    Working demo

--- data-generator/twitch_kafka_producer.py
+++ data-generator/twitch_kafka_producer.py
@@ -11,7 +11,7 @@ client = twitch.TwitchHelix(client_id=<client_id>,
 client.get_oauth()
 
 # Producer instance
-p = Producer({'bootstrap.servers': 'kafka:29092'})
+p = Producer({'bootstrap.servers': 'kafka:9092'})
 
 
 def acked(err, msg):
@@ -34,10 +34,15 @@ try:
     while streams.next:
 
         for stream in islice(streams, 0, 100):
+            arr = stream["tag_ids"]
+            if arr:
+              stream["tag_id"] = arr[0]
+            else:
+              stream["tag_id"] = "dummy-tag"
+            stream["event_time"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
             p.produce('twitch-streams', json.dumps(stream, default=json_serializer, ensure_ascii=False).encode('utf-8'), callback=acked)
             p.poll(0.5)
-
-except Exception, e:
+except:
     print >> sys.stderr, "Exception: %s" % str(e)
     sys.exit(1)
 
